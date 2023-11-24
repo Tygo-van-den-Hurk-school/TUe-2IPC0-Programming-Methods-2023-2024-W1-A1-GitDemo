8cf0040be02086420d12d02ebd4c7eb43547e5f0
Tygo van den Hurk <hazels.05.remount@icloud.com>
2023-11-24 08:51:22 +0100

    Added Counter.java

---
+++ src/main/java/Counter.java
@@ -0,0 +1,30 @@
+/**
+ * A Counter object keeps track of an integer count,
+ * that can be inspected, incremented by one, and
+ * reset to zero. The initial value is zero.
+ *
+ *
+ * @author Tom Verhoeff (TU/e)
+ */
+public class Counter {
+
+    /**
+     * Get the current count.
+     */
+    public long getCount() {
+        return 0;
+    }
+
+    /**
+     * Increment the count by one.
+     */
+    public void increment() {
+    }
+
+    /**
+     * Reset the count to zero.
+     */
+    public void reset() {
+
+    }
+}
